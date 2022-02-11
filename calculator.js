3bb705ce776395c133c881f5576b1e6bad467c80
Kaja Jaronska <kaja.jaronska@gmail.com>
2022-02-11 16:54:39 +0000

    Creating basic calculator functions along with the operate function

--- calculator.js
+++ calculator.js
@@ -1,1 +1,23 @@
-console.log('working?')
+/////////////////////////
+// BASIC CALCULATOR FUNCTIONS
+
+const add = (a,b) => a + b;
+const substract = (a,b) => a-b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
+
+
+const operate = function(firstNum, secondNum, operator) {
+    let result;
+    if(operator==='+') result = add(firstNum,secondNum);
+    if(operator==='-') result = substract(firstNum,secondNum);
+    if(operator==='*') result = multiply(firstNum,secondNum);
+    if(operator==='/') result = divide(firstNum,secondNum);
+    
+    return result;
+
+};
+
+let calculation = operate(10,6,'/');
+
+console.log(calculation);
